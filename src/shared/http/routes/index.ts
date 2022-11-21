5dab78b4d4328524df0ec15331b41de87a3ed33f
emersonacacio <emerson_acacio@lccv.ufal.br>
2022-11-20 23:23:40 -0300

    adicionando a documentação

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express'
+import swaggerUI from 'swagger-ui-express'
 import { filmRoutes } from 'modules/Films/routes/films.router'
-
+import swaggerDocument from '../../../config/swagger.json'
 const routes = Router()
 
 routes.use('/film', filmRoutes)
+routes.use('/docs', swaggerUI.serve)
+routes.get('/docs', swaggerUI.setup(swaggerDocument))
 
 export { routes }
